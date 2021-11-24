5adc5e9d4052400de96310ecd24613c0aa6bb325
Bella <bella.wei@sjsu.edu>
2021-11-23 21:16:06 -0800

    bella: seperarte explore and catch

--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -1,20 +1,23 @@
 import React, { useState, useEffect } from "react"
 import Web3 from "web3"
 import { ABI, ADDRESS } from "../../config"
-import { getPokeobj } from "../pokemon_functions/pokemon"
+import { getPokeobj, getStats } from "../pokemon_functions/pokemon"
 import { getTypes } from "../pokemon_functions/pokemon"
-import { Box, Text, Image, Button, Badge, HStack } from "@chakra-ui/react"
+import { Box, Text, Image, Button, Badge, HStack, Grid } from "@chakra-ui/react"
 import "./main.css"
 import PokemonImage from "../../images/pokemon.png"
 import { useDisclosure } from "@chakra-ui/react"
+import ExploreModal from "./ExploreModal"
+import Child from "./Child"
 
 export default function Main() {
+  const [status, setStatus] = useState("OK")
   const [network, setNetwork] = useState()
   const [account, setAccount] = useState()
   const [gameWorld, setGameWorld] = useState()
   const [species, setSpecies] = useState([])
   const [usersPokemon, setUsersPokemon] = useState()
-  const { isOpen, onOpen, onClose } = useDisclosure()
+  const [explorePokemon, setExplorePokemon] = useState()
 
   const [species_counts, setSpecies_counts] = useState(0)
   const [pokemon_counts, setPokemon_counts] = useState(0)
@@ -45,7 +48,13 @@ export default function Main() {
   }
 
   async function loadWorld(gameWorld) {
-    const species_counts = await gameWorld.methods.unique_species_count().call()
+    const species_counts = await gameWorld.methods
+      .unique_species_count()
+      .call()
+      .catch(() => {
+        setStatus("Did you replace your address and ABI yet?")
+      })
+    setStatus("OK")
     setSpecies_counts(species_counts)
 
     let temp_arr = []
@@ -57,11 +66,11 @@ export default function Main() {
         count: species.count,
         id: species.id,
         poke_info: await getPokeobj(species.pokedex_id),
-        poke_type: await getTypes(species.id),
+        poke_type: await getTypes(species.pokedex_id),
       })
     }
     setSpecies(temp_arr)
-    console.log(temp_arr)
+    console.log("Load world:", temp_arr)
   }
 
   async function loadUsersPokemon(gameWorld, account) {
@@ -91,77 +100,109 @@ export default function Main() {
     console.log(temp_arr)
   }
 
-  async function catch_random_pokemon() {
+  const catch_random_pokemon = async () => {
+    if (status === "Did you replace your address and ABI yet?") {
+      return
+    }
+    if (network && account) {
+      try {
+        let random_pokemon = explorePokemon
+        if (random_pokemon) {
+          //catch
+          //pokedex,hp,att,def,spatt,spdef,sp,lv
+          await gameWorld.methods
+            .caughtPokemon(
+              random_pokemon.id,
+              random_pokemon.pokedex_id,
+              100,
+              100,
+              100,
+              100,
+              100,
+              100,
+              100
+            )
+            .send({ from: account })
+            .then(() => {
+              window.location.reload()
+            })
+        }
+        setStatus("OK")
+      } catch (error) {
+        console.log(error)
+        setStatus("User denied transaction signature!")
+      }
+    } else {
+      setStatus("Please make sure your MetaMask account is connected!")
+    }
+  }
+  function explore_random_pokemon() {
     let random_pokemon = 0
     for (let i = 0; i < 10000; i++) {
       random_pokemon = getRandomInt(species.length)
       if (species[random_pokemon].caught < species[random_pokemon].count) {
         random_pokemon = species[random_pokemon]
-        break
+        setExplorePokemon(random_pokemon)
+        localStorage.setItem("explorePokemon", JSON.stringify(random_pokemon))
+        return random_pokemon
       }
-      random_pokemon = null
     }
-
-    if (random_pokemon) {
-      //catch
-      await gameWorld.methods
-        .caughtPokemon(
-          random_pokemon.pokedex_id,
-          100,
-          100,
-          100,
-          100,
-          100,
-          100,
-          100
-        )
-        .send({ from: account })
-        .then(() => {
-          window.location.reload()
-        })
-    }
+    return null
   }
 
   function getRandomInt(max) {
     return Math.floor(Math.random() * max)
   }
 
+  const connectToMetaMask = async () => {
+    if (window.ethereum) {
+      try {
+        await window.ethereum.request({ method: "eth_requestAccounts" })
+        window.location.reload()
+      } catch (error) {
+        if (error.code === 4001) {
+          console.log(error)
+          setStatus("User rejected MetaMask request!")
+        }
+      }
+    }
+  }
   return (
     <HStack spacing="24px">
+      <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+        <Box marginLeft="15px" marginBottom="5px">
+          <Text color="white"> Status: {status}</Text>
+          <Text color="white">Network: {network}</Text>
+          <Text color="white">Account: {account}</Text>
+          <Button onClick={connectToMetaMask}>Connect</Button>{" "}
+        </Box>
+      </nav>
       <Box className="container-filled">
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <Box
-            className="navbar-brand col-sm-3 col-md-3 mr-0"
-            style={{ color: "white" }}
-          >
-            <p>Account: {account}</p>
-          </Box>
-
-          <Box className="d-flex align-items-center" style={{ color: "white" }}>
-            <p>Network: {network}</p>
-          </Box>
-        </nav>
-
         <Box className="row">
-          <main role="main" className="d-flex justify-content-center">
-            <div className="content mr-auto ml-auto" style={{ opacity: "0.8" }}>
-              <div className="pokemon">
+          <Box role="main" className="d-flex justify-content-center">
+            <Box className="content mr-auto ml-auto" style={{ opacity: "0.8" }}>
+              <Box className="pokemon">
                 <img src={PokemonImage} />
-              </div>
+              </Box>
               <Text fontSize="50px" fontWeight="semibold" color="black">
                 Explore and catch pokemon!
               </Text>
-              <Button
-                background="#00A3C4"
-                size="20px"
-                onClick={() => {
-                  catch_random_pokemon()
-                }}
-              >
-                Explore!
-              </Button>
-            </div>
-          </main>
+              <Box marginBottom="10px">
+                <Button
+                  background="#00A3C4"
+                  size="20px"
+                  onClick={() => {
+                    explore_random_pokemon()
+                  }}
+                >
+                  <ExploreModal
+                    explorePokemondata={explorePokemon}
+                    catch_random_pokemon={() => catch_random_pokemon()}
+                  />
+                </Button>
+              </Box>
+            </Box>
+          </Box>
         </Box>
         <hr></hr>
         <Box className="poke-card" alignItems="center">
@@ -183,6 +224,7 @@ export default function Main() {
                       <Text textTransform="uppercase">
                         {creature.poke_info.name}
                       </Text>
+                      <Text fontSize="36px">{creature.hp}</Text>
                       <Badge borderRadius="full" px="2" background="green">
                         {creature.poke_type}
                       </Badge>
@@ -204,7 +246,8 @@ export default function Main() {
           <Text fontSize="50px" fontWeight="semibold" color="black">
             Your Pokemons:
           </Text>
-          <Box className="d-flex flex-row ">
+
+          <Box className="d-flex flex-row mb=4">
             {usersPokemon &&
               usersPokemon.map((creature, ind) => {
                 return (
