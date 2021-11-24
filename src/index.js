d40cc10042ceddb76e10056e222ec75ca8cc0e73
Bella <bella.wei@sjsu.edu>
2021-11-23 21:17:30 -0800

    bella: set up ChakraProvider at the root

--- src/index.js
+++ src/index.js
@@ -1,22 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import { Route, Link, Routes, BrowserRouter as Router } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.css';
+import React from "react"
+import ReactDOM from "react-dom"
+import "./index.css"
+import { Route, Link, Routes, BrowserRouter as Router } from "react-router-dom"
+import "bootstrap/dist/css/bootstrap.css"
 // Pages
-import MainGame from './pages/main/main'
+import MainGame from "./pages/main/main"
+import { ChakraProvider } from "@chakra-ui/react"
 
 const routing = (
   <Router>
-    <div>
+    <ChakraProvider>
       <Routes>
         <Route exact path="/" element={<MainGame />} />
       </Routes>
-    </div>
+    </ChakraProvider>
   </Router>
 )
 
-ReactDOM.render(
-  routing,
-  document.getElementById('root')
-);
+ReactDOM.render(routing, document.getElementById("root"))
